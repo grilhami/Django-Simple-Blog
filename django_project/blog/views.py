5190f7e62104d249628dc7b04f634039382c816d
Gilang Ramadhan Ilhami <gigilangilhami@gmail.com>
2019-06-18 00:35:21 +0900

    Sample content
    This commit is only to include on
    how to display contesnts from
    django views file using jinja.
    
    - Add a list as an example for
      content database.
    - Modify blog/views.py to include
      the list above in the homepage
    - Modify the home.html to display
      the content in the frontend.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        "author": "Gilang",
+        "title": "Blog Post 1",
+        "content": "This is a content",
+        "date_posted": "June 18, 2019"
+    },
+    {
+        "author": "Gilang",
+        "title": "Blog Post 2",
+        "content": "Second content",
+        "date_posted": "July 18, 2019"
+    }
+]
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        "posts": posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
